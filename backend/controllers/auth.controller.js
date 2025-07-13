d21c21730dd6dbdeb1ae30877ed320546d3ce3bb
Manish Kumar <ms5798034@gmail.com>
2025-07-14 01:55:19 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -34,8 +34,8 @@ import bcrypt from "bcryptjs";
      res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7*24 * 60 * 60 * 1000, // 7 day
-            sameSite: "Strict",
-            secure: false, // Set to true if using HTTPS
+            sameSite: "None",
+            secure: true, // Set to true if using HTTPS
      })
 
      return res.status(201).json({
@@ -76,8 +76,8 @@ import bcrypt from "bcryptjs";
      res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7*24 * 60 * 60 * 1000, // 7 day
-            sameSite: "Strict",
-            secure: false, // Set to true if using HTTPS
+             sameSite: "None",
+            secure: true,  // Set to true if using HTTPS
      })
 
      return res.status(201).json({
